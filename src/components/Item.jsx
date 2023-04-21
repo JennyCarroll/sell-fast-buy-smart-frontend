99aad9705388f4ac406e09b89e3d45f9c408a412
Jason-Wall <jason.waldick@gmail.com>
2023-04-21 10:56:56 -0600

    added fontawesome icon

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,20 +1,27 @@
-import { React } from "react";
-import "./Item.scss";
+import { React } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import './Item.scss';
 
 function Item(props) {
   const bidToDollars = function (value) {
-    return (value / 100).toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD",
+    return (value / 100).toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
     });
   };
 
   return (
     <>
-      <img className="item-image" src={props.photo} alt={props.title} />
-      {props.bid ? (
-        <div className="bid-price">{bidToDollars(props.bid.highest_bid)}</div>
-      ) : null}
+      <img className='item-image' src={props.photo} alt={props.title} />
+      <div className='item-info'>
+        {props.bid && <div className='bid-price'>{bidToDollars(props.bid.highest_bid)}</div>}
+        {props.edit && (
+          <span>
+            <FontAwesomeIcon icon={icon({ name: 'pen-to-square' })} className='bid-price' />
+          </span>
+        )}
+      </div>
     </>
   );
 }
