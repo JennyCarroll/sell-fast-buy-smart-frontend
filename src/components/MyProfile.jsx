820f2a09fadefea3df1a6e6d21db11f8bcfddcc5
Jenny Carroll <jennygc@telus.net>
2023-04-21 13:17:01 -0600

    style font and itemDetail

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,15 +1,15 @@
-import { useParams, Link } from 'react-router-dom';
-import { useEffect, useState, useContext } from 'react';
-import Item from './Item';
-import { loginContext } from '../providers/UserContext';
-import Cookies from 'js-cookie';
-import Rating from './general/Rating';
-import('./MyProfile.scss');
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import Item from "./Item";
+import { loginContext } from "../providers/UserContext";
+import Cookies from "js-cookie";
+import Rating from "./general/Rating";
+import("./MyProfile.scss");
 
 const MyProfile = ({ users, items, images }) => {
   const params = useParams();
   const [thisUser, setThisUser] = useState({});
-  const currentUser = Cookies.get('userId');
+  const currentUser = Cookies.get("userId");
 
   const userId = Number(params.userId);
 
@@ -20,13 +20,17 @@ const MyProfile = ({ users, items, images }) => {
 
   return (
     <body>
-      <div className='profile'>
-        <div className='profile-photo'>
-          {' '}
-          <img className='image' src={'https://i.imgur.com/b0h13kE.jpg'} alt='profile picture' />
-          <h2>{thisUser ? thisUser.name : 'Loading...'}</h2>
+      <div className="profile">
+        <div className="profile-photo">
+          {" "}
+          <img
+            className="image"
+            src={"https://i.imgur.com/b0h13kE.jpg"}
+            alt="profile picture"
+          />
+          <h2>{thisUser ? thisUser.name : "Loading..."}</h2>
         </div>
-        <div className='personal-info'>
+        <div className="personal-info">
           <p>{thisUser && thisUser.email}</p>
           <p>
             {thisUser && thisUser.city}, {thisUser && thisUser.country}
@@ -36,26 +40,38 @@ const MyProfile = ({ users, items, images }) => {
           <Rating user={thisUser}></Rating>
         </div>
       </div>
-      <div className='items'>
+      <div className="items">
         {currentUser ? (
-          <h1>
-            Your Items For Sale
-            <hr />
-          </h1>
+          <div className="titleContainer">
+            <div className="title">
+              <h1>Your Items For Sale</h1>
+            </div>
+            <div className="hr">
+              <hr />
+            </div>
+          </div>
         ) : (
-          <h1>
-            Items For Sale By User
-            <hr />
-          </h1>
+          <div className="titleContainer">
+            <div className="title">
+              <h1>Items For Sale By User</h1>
+            </div>
+            <div className="hr">
+              <hr />
+            </div>
+          </div>
         )}
       </div>
       {/* <div className="items-info"> */}
-      <div className='itemsContainer'>
+      <div className="itemsContainer">
         {itemsForUser.map((item) => {
           let itemBid = items.find((item2) => item2.id === item.id);
           let img = images.find((image) => image.item_id === item.id);
           return (
-            <Link className='itemLink' to={`/items/${item.id}/edit`} key={item.id}>
+            <Link
+              className="itemLink"
+              to={`/items/${item.id}/edit`}
+              key={item.id}
+            >
               <Item photo={img.img_url} title={item.title} bid={itemBid}></Item>
             </Link>
           );
