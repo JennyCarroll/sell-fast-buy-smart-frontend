27ad221844eb289e74e1de13ff75d975171528c8
Jenny Carroll <jennygc@telus.net>
2023-05-06 15:26:14 -0600

    add short circuting to .maps to prevent errors

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,64 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Item from './Item';
-import './Items.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import Item from "./Item";
+import "./Items.scss";
 
 function Items(props) {
-  let arrayOfItemPhotos = props.images.map((image) => {
-    let item = props.items.find((item) => item.id === image.item_id);
-    let itemBid = props.items.find((item2) => item2.id === item.id);
-    return (
-      <div key={image.item_id} className='item-container'>
-        <Link className='item-link' to={'/items/' + image.item_id}>
-          <Item photo={image.img_url} title={item.title} bid={itemBid}></Item>
-        </Link>
-      </div>
-    );
-  });
+  let arrayOfItemPhotos =
+    Array.isArray(props.images) &&
+    props.images.map((image) => {
+      let item = props.items.find((item) => item.id === image.item_id);
+      let itemBid = props.items.find((item2) => item2.id === item.id);
+      return (
+        <div key={image.item_id} className="item-container">
+          <Link className="item-link" to={"/items/" + image.item_id}>
+            <Item photo={image.img_url} title={item.title} bid={itemBid}></Item>
+          </Link>
+        </div>
+      );
+    });
 
   const bidToDollars = function (value) {
-    return (value / 100).toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD',
+    return (value / 100).toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
     });
   };
 
   return (
-    <div className='top-element'>
-      <h1 className='site-name'>Sell Fast Buy Smart</h1>
+    <div className="top-element">
+      <h1 className="site-name">Sell Fast Buy Smart</h1>
 
-      <div className='title-container'>
-        <div className='title'>
+      <div className="title-container">
+        <div className="title">
           <h1>Ending Soon!</h1>
         </div>
-        <div className='hr'>
+        <div className="hr">
           <hr />
         </div>
       </div>
 
-      <div className='ending-soon'>
-        {props.endingSoon.slice(0, 8).map((item) => {
-          let image = props.images.find((image) => image.item_id === item.id);
-          let itemBid = props.items.find((item2) => item2.id === item.id);
-          return (
-            <Link key={item.id} className='item-container item-link' to={`/items/${item.id}`}>
-              <div>
-                <img className='featured-image' src={image.img_url} alt={item.title} />
-                <div className='bid-price'>{bidToDollars(itemBid.highest_bid)}</div>
-              </div>
-            </Link>
-          );
-        })}
+      <div className="ending-soon">
+        {Array.isArray(props.endingSoon) &&
+          props.endingSoon.slice(0, 8).map((item) => {
+            let image = props.images.find((image) => image.item_id === item.id);
+            let itemBid = props.items.find((item2) => item2.id === item.id);
+            return (
+              <Link
+                key={item.id}
+                className="item-container item-link"
+                to={`/items/${item.id}`}
+              >
+                <div>
+                  <img
+                    className="featured-image"
+                    src={image.img_url}
+                    alt={item.title}
+                  />
+                  <div className="bid-price">
+                    {bidToDollars(itemBid.highest_bid)}
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
       </div>
-      <div className='title-container'>
-        <div className='title'>
+      <div className="title-container">
+        <div className="title">
           <h1>All Items</h1>
         </div>
-        <div className='hr'>
+        <div className="hr">
           <hr />
         </div>
       </div>
-      <div className='items-container'>{arrayOfItemPhotos}</div>
+      <div className="items-container">{arrayOfItemPhotos}</div>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
-import './Nav.scss';
-import Search from './Search';
-import { Link } from 'react-router-dom';
-import { useContext, useState, useRef } from 'react';
-import { loginContext } from '../providers/UserContext';
-import { stateContext } from '../providers/StateContext';
+import "./Nav.scss";
+import Search from "./Search";
+import { Link } from "react-router-dom";
+import { useContext, useState, useRef } from "react";
+import { loginContext } from "../providers/UserContext";
+import { stateContext } from "../providers/StateContext";
 
 function Nav(props) {
   let categories = props.categories;
@@ -21,17 +21,17 @@ function Nav(props) {
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       login(userId);
     }
   };
 
   return (
-    <div className='nav nav-bar'>
-      <div className='nav top-nav'>
-        <div className='nav logo btn'>
+    <div className="nav nav-bar">
+      <div className="nav top-nav">
+        <div className="nav logo btn">
           <Link
-            to={'/'}
+            to={"/"}
             onClick={() => {
               setStateRefresh(true);
             }}
@@ -39,48 +39,62 @@ function Nav(props) {
             <h1>SFBS</h1>
           </Link>
         </div>
-        <div className='nav search'>
+        <div className="nav search">
           <Search items={props.items} />
         </div>
-        <div className='nav right-nav'>
+        <div className="nav right-nav">
           {currentUser ? (
             <>
-              <div className=''>
-                <Link to={'/items/new'}>
+              <div className="">
+                <Link to={"/items/new"}>
                   <p>Sell</p>
                 </Link>
               </div>
-              <div className='dropdown'>
+              <div className="dropdown">
                 <p>Profile</p>
-                <div className='dropdown-content'>
-                  <Link className='btn btn-light optio' to={`/profile/${currentUser}`}>
+                <div className="dropdown-content">
+                  <Link
+                    className="btn btn-light optio"
+                    to={`/profile/${currentUser}`}
+                  >
                     Profile
                   </Link>
-                  <Link className='btn btn-light option' to={`/`} onClick={() => logout()}>
+                  <Link
+                    className="btn btn-light option"
+                    to={`/`}
+                    onClick={() => logout()}
+                  >
                     logout
                   </Link>
                 </div>
               </div>
-              <div className=''>
+              <div className="">
                 <Link to={`bids/${currentUser}`}>
                   <p>Bids</p>
                 </Link>
               </div>
             </>
           ) : (
-            <div className='dropdown'>
-              <button onMouseEnter={bringInputToFocus} className='btn btn-light login login'>
+            <div className="dropdown">
+              <button
+                onMouseEnter={bringInputToFocus}
+                className="btn btn-light login login"
+              >
                 Login / Sign Up
               </button>
-              <div className='dropdown-content'>
+              <div className="dropdown-content">
                 <input
                   ref={inputRef}
                   onKeyDown={handleKeyDown}
-                  className='login form-control option'
-                  placeholder='email'
+                  className="login form-control option"
+                  placeholder="email"
                   onChange={handleChange}
                 />
-                <button className='btn btn-light option' id='login' onClick={() => login(userId)}>
+                <button
+                  className="btn btn-light option"
+                  id="login"
+                  onClick={() => login(userId)}
+                >
                   Login
                 </button>
               </div>
@@ -88,7 +102,9 @@ function Nav(props) {
           )}
           <div>
             <i
-              className={props.theme ? 'bi bi-lightbulb btn' : 'bi bi-lightbulb-off btn'}
+              className={
+                props.theme ? "bi bi-lightbulb btn" : "bi bi-lightbulb-off btn"
+              }
               onClick={() => {
                 let theme = !props.theme;
                 props.setTheme(theme);
@@ -97,13 +113,18 @@ function Nav(props) {
           </div>
         </div>
       </div>
-      <div className='nav bottom-nav'>
-        <div className='nav categories'>
-          {categories.map((category) => (
-            <Link key={category.id} to={`/categories/${category.id}`} className='category'>
-              <h2>{category.title}</h2>
-            </Link>
-          ))}
+      <div className="nav bottom-nav">
+        <div className="nav categories">
+          {Array.isArray(categories) &&
+            categories.map((category) => (
+              <Link
+                key={category.id}
+                to={`/categories/${category.id}`}
+                className="category"
+              >
+                <h2>{category.title}</h2>
+              </Link>
+            ))}
         </div>
       </div>
     </div>
